4053c524d94e2bdb2fbe16d83ffa1ecbb92371ac
KostadinovK <kostadinovk2001@gmail.com>
2019-03-11 21:05:03 +0200

    Finished the lab to lecture 04

---
+++ 04-Interfaces-And-Abstraction/Lab/02-Cars/Seat.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    class Seat : ICar
+    {
+        public string Model { get; set; }
+        public string Color { get; set; }
+        public Seat(string model, string color)
+        {
+            Model = model;
+            Color = color;
+        }
+
+        public string Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Stop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"{Color} Seat {Model}");
+            sb.AppendLine("Engine start");
+            sb.Append("Breaaak!");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ 04-Interfaces-And-Abstraction/Lab/02-Cars/Tesla.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cars
+{
+    class Tesla : ICar, IElectricCar
+    {
+        public string Model { get; set; }
+        public string Color { get; set; }
+
+        public int Battery { get; set; }
+
+        public Tesla(string model, string color, int battery)
+        {
+            Model = model;
+            Color = color;
+            Battery = battery;
+        }
+
+        public string Start()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string Stop()
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"{Color} Tesla {Model} with {Battery} Batteries");
+            sb.AppendLine("Engine start");
+            sb.Append("Breaaak!");
+
+            return sb.ToString();
+        }
+    }
+}
