64e78af1cb4be4a10a1898e7a009b7320be08655
KostadinovK <kostadinovk2001@gmail.com>
2019-03-14 20:40:02 +0200

    Finished the lab for lecture 05

---
+++ 05-Polymorphism/Lab/01-MathOperation/MathOperations.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Operations
+{
+    public class MathOperations
+    {
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public double Add(double a, double b, double c)
+        {
+            return a + b + c;
+        }
+
+        public decimal Add(decimal a, decimal b, decimal c)
+        {
+            return a + b + c;
+        }
+    }
+}
