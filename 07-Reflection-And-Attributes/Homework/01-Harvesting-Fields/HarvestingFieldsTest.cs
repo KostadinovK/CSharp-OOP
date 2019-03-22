c343e99643bbd8c8096ad51503a84cc2798c9430
KostadinovK <kostadinovk2001@gmail.com>
2019-03-22 14:37:31 +0200

    Commited to pull from pc

---
+++ 07-Reflection-And-Attributes/Homework/01-Harvesting-Fields/HarvestingFieldsTest.cs
@@ -0,0 +1,66 @@
+﻿ using System.Linq;
+ using System.Reflection;
+
+ namespace P01_HarvestingFields
+{
+    using System;
+
+    public class HarvestingFieldsTest
+    {
+        public static void Main()
+        {
+            string line = Console.ReadLine();
+
+            while (line != "HARVEST")
+            {
+                PrintAllFieldsOfType(line);
+
+                line = Console.ReadLine();
+            }
+        }
+
+        private static void PrintAllFieldsOfType(string type)
+        {
+            Type classType = typeof(HarvestingFields);
+
+            FieldInfo[] fields = null;
+
+            if (type == "public")
+            {
+                fields = classType.GetFields(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public);
+            }
+            else if (type == "protected")
+            {
+                fields = classType.GetFields(BindingFlags.Instance | BindingFlags.Static | BindingFlags.NonPublic)
+                    .Where(f => f.Attributes == FieldAttributes.Family).ToArray();
+            }
+            else if(type == "private")
+            {
+                fields = classType.GetFields(BindingFlags.Static | BindingFlags.Instance | BindingFlags.NonPublic)
+                    .Where(f => f.Attributes == FieldAttributes.Private).ToArray();
+            }
+            else if(type == "all")
+            {
+                fields = classType.GetFields(BindingFlags.Static | BindingFlags.Instance | BindingFlags.NonPublic |
+                                             BindingFlags.Public);
+            }
+
+            if (fields == null)
+            {
+                throw new InvalidOperationException();
+            }
+
+            foreach (var field in fields)
+            {
+                if (field.Attributes == FieldAttributes.Family)
+                {
+                    Console.WriteLine($"protected {field.FieldType.Name} {field.Name}");
+                }
+                else
+                {
+                    Console.WriteLine($"{field.Attributes.ToString().ToLower()} {field.FieldType.Name} {field.Name}");
+                }
+            }
+        }
+    }
+}
---
+++ 07-Reflection-And-Attributes/Homework/02-BlackBoxInteger/BlackBoxIntegerTests.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+using System.Linq;
+using System.Reflection;
+
+public class BlackBoxIntegerTests
+{
+    public static void Main()
+    {
+        string line = Console.ReadLine();
+
+        var type = typeof(BlackBoxInteger);
+        var instance = (BlackBoxInteger)Activator.CreateInstance(type, true);
+        var result = type.GetField("innerValue");
+        while (line != "END")
+        {
+            string[] commandInfo = line.Split("_").ToArray();
+            string command = commandInfo[0];
+            int param = int.Parse(commandInfo[1]);
+
+            var method = type.GetMethod(command);
+
+            method.Invoke(instance, new object[]{param});
+            Console.WriteLine(result.Name);
+
+            line = Console.ReadLine();
+        }
+    }
+}
+
